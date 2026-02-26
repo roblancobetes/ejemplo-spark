42c25e1aa7fe4edd64d87190b71fd17365505caa
roblancobetes <rodrigoblancobetes@gmail.com>
2026-02-26 11:20:47 +0100

    Update Main.scala

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -5,16 +5,7 @@ import org.apache.spark.sql.SparkSession
 object Main {
   def main(args: Array[String]): Unit = {
 
-    val spark = SparkSession.builder()
-      .appName("ejemplo-spark")
-      .master("local[*]")
-      .getOrCreate()
-
-  val df = spark.read
-    .option("header", value = true)
-    .csv("data/AAPL.csv")
-
-  df.show()
+ println("Hello World")
 
   }
 }
